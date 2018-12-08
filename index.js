c490b6d393a8c42c0f6148616518d155d178d66d
Nick Koutrelakos <wkoutre@gmail.com>
2018-12-08 12:14:37 -0800

    Added UNManager settings for using LayoutAnimation with Android

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import { AppRegistry } from "react-native";
+import { AppRegistry, UIManager, Platform } from "react-native";
 import App from "./App.tsx";
 import { name as appName } from "./app.json";
+
+if (Platform.OS !== "ios" && UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
 AppRegistry.registerComponent(appName, () => App);
